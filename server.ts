e606166850534db871f66c24f49e207e97e89add
James Prentice <jamesprenticez@gmail.com>
2024-10-02 17:38:59 +1300

    index.html?

--- server.ts
+++ server.ts
@@ -2,48 +2,39 @@ import path from "path";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-
-import pageRoutes from "./routes/pages";
+// import pageRoutes from "./routes/pages";
 import apiRoutes from "./routes/api";
-
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./public/swagger.json";
 
 // Initialize express
 const app = express();
 
-// Allow express to understand cookies (required for HttpOnly)
-app.use(cookieParser());
-
-// Middleware for JSON parsing
-app.use(express.json());
-
-// Cross Origin
-app.use(
-  cors({
-    origin: process.env.BASE_URL,
-    credentials: true,
-  }),
-);
-
-// Set JSON formatting
-app.set("json spaces", 2);
+// Middleware
+app.use(cookieParser()); // For parsing cookies
+app.use(express.json()); // For parsing JSON bodies
+app.use(cors({ origin: process.env.BASE_URL, credentials: true })); // CORS setup
+app.set("json spaces", 2); // Pretty JSON formatting
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Serve swagger api docs
+// Serve index.html for the root URL
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Serve Swagger API docs
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-// Start the server only if the database connection is successful
-const PORT = process.env.PORT || 5000;
+// // Serve HTML pages
+// app.use(pageRoutes); // Make sure this is before apiRoutes to handle HTML routes
 
-// Server html
-app.use(pageRoutes);
-
-// Use routes
+// Use API routes
 app.use(apiRoutes);
 
+// Start the server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
